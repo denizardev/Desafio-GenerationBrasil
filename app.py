fa86579f9bcd1bec1cb532623c4a3ae1d9a80799
Denizard <denizard.oliveira@gmail.com>
2024-09-13 09:39:25 -0300

    app.py change route 127 > 0

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from db_config import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Obtem a porta fornecida pelo Render
+    app.run(host='0.0.0.0', port=port, debug=True)
